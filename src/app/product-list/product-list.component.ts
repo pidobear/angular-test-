b7d2be32b00046ae102783b72a91db99d476cfe5
pidobear <pidobear@live.com>
2022-06-18 00:29:21 +0800

    test auto type text box

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,13 +9,52 @@ import { products } from '../products';
 })
 export class ProductListComponent {
   products = products;
-
+  day: number = 0;
+  
   share() {
     window.alert('The product has been shared!');
   }
 
   nextLine() {
-    document.getElementById('textbox')!.textContent = 'This text is different!';
+    this.day++;
+    //document.getElementById('textbox')!.textContent = 'I like being a dog!';
+    if (this.day == 6) {
+      this.day = 0;
+    }
+
+    switch (this.day) {
+      case 1:
+        document.getElementById('textbox')!.textContent =
+          'I like being a dog1!';
+        break;
+      case 2:
+        document.getElementById('textbox')!.textContent =
+          'I like being a dog2!';
+        break;
+      case 3:
+        document.getElementById('textbox')!.textContent =
+          'I like being a dog3!';
+        break;
+      case 4:
+        document.getElementById('textbox')!.textContent =
+          'I like being a dog4!';
+        break;
+      case 5:
+        document.getElementById('textbox')!.textContent =
+          'I like being a dog5!';
+        break;
+      case 6:
+        document.getElementById('textbox')!.textContent =
+          'I like being a dog6!';
+        break;
+      default:
+        console.log('No such day exists!');
+        break;
+    }
+    var el = document.getElementById('textbox');
+    el!.style.animation = 'none';
+    el!.offsetHeight; /* trigger reflow */
+    el!.style.animation = null!;
   }
 }
 
